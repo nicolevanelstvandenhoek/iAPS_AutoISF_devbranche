4bd48c4cf2a1045b8ca4c9adaffcfc1ea7344962
Robert <comps@diprobe.com>
2024-05-19 21:43:26 +0200

    align cgm & pump config NavBar

--- FreeAPS/Sources/Modules/CGM/View/CGMRootView.swift
+++ FreeAPS/Sources/Modules/CGM/View/CGMRootView.swift
@@ -30,124 +30,122 @@ extension CGM {
         // @AppStorage(UserDefaults.BTKey.cgmTransmitterDeviceAddress.rawValue) private var cgmTransmitterDeviceAddress: String? = nil
 
         var body: some View {
-            NavigationView {
-                Form {
-                    Section(header: Text("CGM")) {
-                        Picker("Type", selection: $state.cgmCurrent) {
-                            ForEach(state.listOfCGM) { type in
-                                VStack(alignment: .leading) {
-                                    Text(type.displayName)
-                                    Text(type.subtitle).font(.caption).foregroundColor(.secondary)
-                                }.tag(type)
-                            }
-                        }
-                        if let link = state.cgmCurrent.type.externalLink {
-                            Button("About this source") {
-                                UIApplication.shared.open(link, options: [:], completionHandler: nil)
-                            }
-                        }
-                    }
-                    if state.cgmCurrent.type == .plugin {
-                        Section {
-                            Button("CGM Configuration") {
-                                setupCGM.toggle()
-                            }
-                        }
-                    }
-                    if state.cgmCurrent.type == .xdrip {
-                        Section(header: Text("Heartbeat")) {
+            Form {
+                Section(header: Text("CGM")) {
+                    Picker("Type", selection: $state.cgmCurrent) {
+                        ForEach(state.listOfCGM) { type in
                             VStack(alignment: .leading) {
-                                if let cgmTransmitterDeviceAddress = state.cgmTransmitterDeviceAddress {
-                                    Text("CGM address :")
-                                    Text(cgmTransmitterDeviceAddress)
-                                } else {
-                                    Text("CGM is not used as heartbeat.")
-                                }
-                            }
+                                Text(type.displayName)
+                                Text(type.subtitle).font(.caption).foregroundColor(.secondary)
+                            }.tag(type)
                         }
                     }
-                    if state.cgmCurrent.type == .plugin && state.cgmCurrent.id.contains("Libre") {
-                        Section(header: Text("Calibrations")) {
-                            Text("Calibrations").navigationLink(to: .calibrations, from: self)
+                    if let link = state.cgmCurrent.type.externalLink {
+                        Button("About this source") {
+                            UIApplication.shared.open(link, options: [:], completionHandler: nil)
                         }
                     }
-
-                    if state.cgmCurrent.type == .nightscout {
-                        Section(header: Text("Nightscout")) {
-                            if state.url != nil {
-                                Button(state.url!.absoluteString) {
-                                    UIApplication.shared.open(state.url!, options: [:], completionHandler: nil)
-                                }
+                }
+                if state.cgmCurrent.type == .plugin {
+                    Section {
+                        Button("CGM Configuration") {
+                            setupCGM.toggle()
+                        }
+                    }
+                }
+                if state.cgmCurrent.type == .xdrip {
+                    Section(header: Text("Heartbeat")) {
+                        VStack(alignment: .leading) {
+                            if let cgmTransmitterDeviceAddress = state.cgmTransmitterDeviceAddress {
+                                Text("CGM address :")
+                                Text(cgmTransmitterDeviceAddress)
                             } else {
-                                Text("You need to configure Nightscout URL")
+                                Text("CGM is not used as heartbeat.")
                             }
                         }
                     }
+                }
+                if state.cgmCurrent.type == .plugin && state.cgmCurrent.id.contains("Libre") {
+                    Section(header: Text("Calibrations")) {
+                        Text("Calibrations").navigationLink(to: .calibrations, from: self)
+                    }
+                }
 
-                    Section(header: Text("Calendar")) {
-                        Toggle("Create Events in Calendar", isOn: $state.createCalendarEvents)
-                        if state.calendarIDs.isNotEmpty {
-                            Picker("Calendar", selection: $state.currentCalendarID) {
-                                ForEach(state.calendarIDs, id: \.self) {
-                                    Text($0).tag($0)
-                                }
+                if state.cgmCurrent.type == .nightscout {
+                    Section(header: Text("Nightscout")) {
+                        if state.url != nil {
+                            Button(state.url!.absoluteString) {
+                                UIApplication.shared.open(state.url!, options: [:], completionHandler: nil)
                             }
-                            Toggle("Display Emojis as Labels", isOn: $state.displayCalendarEmojis)
-                            Toggle("Display IOB and COB", isOn: $state.displayCalendarIOBandCOB)
-                        } else if state.createCalendarEvents {
-                            if #available(iOS 17.0, *) {
-                                Text(
-                                    "If you are not seeing calendars to choose here, please go to Settings -> iAPS -> Calendars and change permissions to \"Full Access\""
-                                ).font(.footnote)
+                        } else {
+                            Text("You need to configure Nightscout URL")
+                        }
+                    }
+                }
 
-                                Button("Open Settings") {
-                                    // Get the settings URL and open it
-                                    if let url = URL(string: UIApplication.openSettingsURLString) {
-                                        UIApplication.shared.open(url)
-                                    }
+                Section(header: Text("Calendar")) {
+                    Toggle("Create Events in Calendar", isOn: $state.createCalendarEvents)
+                    if state.calendarIDs.isNotEmpty {
+                        Picker("Calendar", selection: $state.currentCalendarID) {
+                            ForEach(state.calendarIDs, id: \.self) {
+                                Text($0).tag($0)
+                            }
+                        }
+                        Toggle("Display Emojis as Labels", isOn: $state.displayCalendarEmojis)
+                        Toggle("Display IOB and COB", isOn: $state.displayCalendarIOBandCOB)
+                    } else if state.createCalendarEvents {
+                        if #available(iOS 17.0, *) {
+                            Text(
+                                "If you are not seeing calendars to choose here, please go to Settings -> iAPS -> Calendars and change permissions to \"Full Access\""
+                            ).font(.footnote)
+
+                            Button("Open Settings") {
+                                // Get the settings URL and open it
+                                if let url = URL(string: UIApplication.openSettingsURLString) {
+                                    UIApplication.shared.open(url)
                                 }
                             }
                         }
                     }
+                }
 
-                    Section(header: Text("Experimental")) {
-                        Toggle("Smooth Glucose Value", isOn: $state.smoothGlucose)
-                    }
+                Section(header: Text("Experimental")) {
+                    Toggle("Smooth Glucose Value", isOn: $state.smoothGlucose)
                 }
-                .scrollContentBackground(.hidden).background(color)
-                .onAppear(perform: configureView)
-                .navigationTitle("CGM")
-                .navigationBarTitleDisplayMode(.automatic)
-                .navigationBarItems(leading: displayClose ? Button("Close", action: state.hideModal) : nil)
-                .sheet(isPresented: $setupCGM) {
-                    if let cgmFetchManager = state.cgmManager,
-                       let cgmManager = cgmFetchManager.cgmManager,
-                       state.cgmCurrent.type == cgmFetchManager.cgmGlucoseSourceType,
-                       state.cgmCurrent.id == cgmFetchManager.cgmGlucosePluginId
-                    {
-                        CGMSettingsView(
-                            cgmManager: cgmManager,
-                            bluetoothManager: state.provider.apsManager.bluetoothManager!,
-                            unit: state.settingsManager.settings.units,
-                            completionDelegate: state
-                        )
-                    } else {
-                        CGMSetupView(
-                            CGMType: state.cgmCurrent,
-                            bluetoothManager: state.provider.apsManager.bluetoothManager!,
-                            unit: state.settingsManager.settings.units,
-                            completionDelegate: state,
-                            setupDelegate: state,
-                            pluginCGMManager: self.state.pluginCGMManager
-                        )
-                    }
+            }
+            .scrollContentBackground(.hidden).background(color)
+            .onAppear(perform: configureView)
+            .navigationTitle("CGM")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(leading: displayClose ? Button("Close", action: state.hideModal) : nil)
+            .sheet(isPresented: $setupCGM) {
+                if let cgmFetchManager = state.cgmManager,
+                   let cgmManager = cgmFetchManager.cgmManager,
+                   state.cgmCurrent.type == cgmFetchManager.cgmGlucoseSourceType,
+                   state.cgmCurrent.id == cgmFetchManager.cgmGlucosePluginId
+                {
+                    CGMSettingsView(
+                        cgmManager: cgmManager,
+                        bluetoothManager: state.provider.apsManager.bluetoothManager!,
+                        unit: state.settingsManager.settings.units,
+                        completionDelegate: state
+                    )
+                } else {
+                    CGMSetupView(
+                        CGMType: state.cgmCurrent,
+                        bluetoothManager: state.provider.apsManager.bluetoothManager!,
+                        unit: state.settingsManager.settings.units,
+                        completionDelegate: state,
+                        setupDelegate: state,
+                        pluginCGMManager: self.state.pluginCGMManager
+                    )
                 }
-                .onChange(of: setupCGM) { setupCGM in
-                    state.setupCGM = setupCGM
-                }
-                .onChange(of: state.setupCGM) { setupCGM in
-                    self.setupCGM = setupCGM
-                }
+            }
+            .onChange(of: setupCGM) { setupCGM in
+                state.setupCGM = setupCGM
+            }
+            .onChange(of: state.setupCGM) { setupCGM in
+                self.setupCGM = setupCGM
             }
         }
     }
